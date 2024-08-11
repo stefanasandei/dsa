f4bde28d9468c57e6d7cd5f3734c58acf889d87e
Asandei Stefan <asandei.stefanel@gmail.com>
2024-08-11 16:17:27 +0300

    dsa lib

---
+++ lib/dsa.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Asandei Stefan on 11.08.2024.
+//
+
+#include "dsa.hpp"
+
+#include <cstdio>
+
+namespace dsa {
+
+    void hello() {
+        printf("hello world\n");
+    }
+
+}
---
+++ lib/dsa.hpp
@@ -0,0 +1,11 @@
+//
+// Created by Asandei Stefan on 11.08.2024.
+//
+
+#pragma once
+
+namespace dsa {
+
+    void hello();
+
+}
